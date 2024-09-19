cc2427b082f0d33c6ad7f7911e2b8836274baf2e
ummesalma303 <ummesalma333999@gmail.com>
2024-09-19 13:14:48 +0600

    added login page

---
+++ payoo-mfs/payoo-mfs/js/home.js
@@ -0,0 +1,17 @@
+
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+    event.preventDefault()
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    const balanceNumber = parseFloat(addMoneyInput)
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    const accountBalance = document.getElementById('account-balance').innerText;
+    const balanceAccount = parseFloat(accountBalance)
+
+    if (pinNumberInput.length === 4) {
+        const newBalance = balanceNumber + balanceAccount;
+        document.getElementById('account-balance').innerText = newBalance
+    } else {
+        alert('Failed to add money! Please try again.')
+    }
+})
---
+++ payoo-mfs/payoo-mfs/js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('button-login')
+    .addEventListener('click', function (event) {
+    event.preventDefault()
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+        
+    if (phoneNumber === '5' && pinNumber ==='1234') {
+        console.log('you are logged in');
+        window.location.href = "/home.html";
+    } else {
+        alert('Wrong phone number or pin. you must give the number of 5 digit')
+    }
+})
