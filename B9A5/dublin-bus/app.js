a3aa68841a5cbb865fe0bcff8d5f71166bdde005
ummesalma303 <ummesalma333999@gmail.com>
2024-09-21 20:17:21 -0700

    disable next button based on phone number  length and reset window

--- B9A5/dublin-bus/app.js
+++ B9A5/dublin-bus/app.js
@@ -3,7 +3,11 @@ const totalBooked = document.getElementById('total-booked');
 const availableSeat = document.getElementById('available-seat');
 const totalPriceEl = document.getElementById('total-price');
 const couponField = document.getElementById('coupon-field');
-const couponBtn = document.getElementById('coupon-btn')
+const couponBtn = document.getElementById('coupon-btn');
+const defaultText = document.getElementById('default-text');
+const grandTotal = document.getElementById('grand-total');
+const phoneNumber = document.getElementById('phone-number');
+const nextBtn = document.getElementById('nextButton')
 const selectedSeat = [];
 let totalPrice = 0;
 function handleSelectSeat(event) {
@@ -18,7 +22,9 @@ function handleSelectSeat(event) {
     // decrease available seat value
     const availableSeatValue = parseFloat(availableSeat.innerText) - 1;
     availableSeat.innerText = availableSeatValue;
-
+  // remove default text
+   defaultText.classList.add('hidden')
+        
     totalPrice += 550;
     totalPriceEl.innerText = totalPrice.toFixed(2)
     seatSelectedEl.innerHTML += `
@@ -32,9 +38,43 @@ function handleSelectSeat(event) {
         couponField.removeAttribute('disabled');
         couponBtn.removeAttribute('disabled');
 
-    }
+        }
+      
     } else {
+        
         return alert('maximum seat booked')
     }
-    
 }
+// coupon button function
+couponBtn.addEventListener('click',function () {
+    const couponInputValue = couponField.value;
+    let couponSave = 0;
+    if (couponInputValue !== 'NEW50'&&couponInputValue !== 'Couple 20') {
+        alert('Your provide code is not valid');
+        return
+    }
+     if (couponInputValue === 'NEW50') {
+         couponSave = totalPrice * 0.15;
+    }else if (couponInputValue === 'Couple 20') {
+         couponSave = totalPrice * 0.20;
+    }
+     const grandTotalValue = totalPrice - couponSave;
+    grandTotal.innerText = grandTotalValue.toFixed(2);
+
+    // hide coupon input field and apply button
+    const showCouponPrice = document.getElementById('show-coupon-price');
+    showCouponPrice.innerHTML = `<p>Discount</p> <p><span>-BDT: </span> <span>${couponSave}</span></p>`
+})
+
+phoneNumber.addEventListener('input',function () {
+    const phoneNumberValue = phoneNumber.value;
+    if (phoneNumberValue.length >= 11) {
+        nextBtn.removeAttribute('disabled')
+    } else {
+        nextBtn.setAttribute('disabled',true)
+    }
+})
+
+document.getElementById('btn-continue').addEventListener('click',function () {
+    location.reload()
+})
